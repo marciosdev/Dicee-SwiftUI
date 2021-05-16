ecec5d83528625d2ca013589961d91681b2220c9
Marcio <marcio@touchable.dev>
2021-05-15 21:41:35 -0300

    Initial Commit

---
+++ Dicee-SwiftUI/Dicee_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dicee_SwiftUIApp.swift
+//  Dicee-SwiftUI
+//
+//  Created by Marcio Silva on 15/05/21.
+//
+
+import SwiftUI
+
+@main
+struct Dicee_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
